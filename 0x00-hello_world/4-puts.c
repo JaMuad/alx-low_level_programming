870323a8e716eaec5b920b22b695cdf8f96dec2e
Muad <muadmaalim@gmail.com>
2022-09-08 18:23:44 -0700

    1st c code sing puts and on vim

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * we are printing main- Printing
+ *
+ * this program then will print out the text Programming is like building a multilingual puzzle
+ *
+ * Return should always 0 (when successful)
+ *
+ */
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
