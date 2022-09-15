ec8fcd13e1e336fa2369c7c40e4328df4e6732c4
Muad <muadmaalim@gmail.com>
2022-09-14 19:37:35 -0700

    try2x task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - print putchar follwoed new line
+ * main - print putchar
  * Return: always 0
  */
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,6 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
