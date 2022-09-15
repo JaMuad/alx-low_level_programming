67abd207751610915bf9cd8126df5c8a2bcdad9d
Muad <muadmaalim@gmail.com>
2022-09-14 19:33:52 -0700

    try again task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED 
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,10 +11,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-
-endif
-
-
+void num(int r, int n);
+#endif
