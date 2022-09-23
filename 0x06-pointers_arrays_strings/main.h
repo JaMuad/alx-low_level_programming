9f167fa626783d121e269d6ca2a465aeaec8b1cb
Muad <muadmaalim@gmail.com>
2022-09-23 21:46:32 +0300

    com

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * File: main.h
- * Auth: JaMuad(Muad)
- * Desc: Header file containing prototypes for all functions
- * used in the 0x06. C - More pointers, arrays and strings directory
- */
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 
 #endif
