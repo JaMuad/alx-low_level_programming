4d0d046a1f425021abb4dcc99b90d19a00202b0a
Muad <muadmaalim@gmail.com>
2022-09-23 22:52:23 +0300

    reverse

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,32 @@
 #include "main.h"
 
 /**
- * _strcat - combines two strings
- * @dest - first string
- * @src - Second string
- *
- * Return: char*
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int len = 0;
-	int len2 = 0;
+	int i;
+	int j;
 
-	while (*(dest + len))
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		len++;
+		i++;
 	}
-	while(*(src + len2))
+
+	while (src[j] != '\0')
 	{
-		dest[len] = *(src + len2);
-		len++;
-		len2++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
+
+	dest[i] = '\0';
 	return (dest);
 }
